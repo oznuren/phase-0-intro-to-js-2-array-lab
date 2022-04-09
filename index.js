ec53627774a0709837bead2d3867d6c4537987fd
Öznur <oznur@tuta.io>
2022-04-09 12:33:55 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+  cats.push(name);
+  return cats;
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+  return cats;
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+  return cats;
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+  return cats;
+}
+
+function appendCat(name) {
+  let newCats = cats.slice();
+  newCats.push("Broom");
+  return newCats;
+}
+
+function prependCat(name) {
+  let newerCats = cats.slice();
+  newerCats.unshift("Arnold");
+  return newerCats;
+}
+
+function removeLastCat() {
+  let catsPop = cats.slice();
+  catsPop.pop();
+  return catsPop;
+}
+
+function removeFirstCat() {
+  let catsSh = cats.slice();
+  catsSh.shift();
+  return catsSh;
+}
